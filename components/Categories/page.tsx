9c2bf3635a98f498411f0918ebc80966563529a5
ZiyahulAnsath <91413647+ZiyahulAnsath@users.noreply.github.com>
2024-02-08 01:59:56 +0530

    set the color categories page

--- components/Categories/page.tsx
+++ components/Categories/page.tsx
@@ -6,9 +6,9 @@ export default function Categories() {
   return (
     <>
       {categoriesData.map((item) => (
-        <div key={item.id}>
+        <div key={item.id} className="transform transition-transform hover:scale-105 hover:shadow-md hover:shadow-gray-bg_categories">
           <Link href={`category/${item.name}`}>
-            <div className="flex md:gap-8 items-center bg-gray-bg_categories hover:bg-gradient-to-tr from-green-text  to-green-background ease-in-out duration-150  hover:text-gray-bg_primary hover:font-bold border-none rounded-r-md mb-2">
+            <div className="flex md:gap-8 items-center bg-gray-bg_categories  ease-in-out duration-300 hover:bg-green-text hover:text-gray-bg_primary text-gray-text border-none rounded-r-md mb-3">
               <div className="mx-auto">
                 <Image
                   src={"/alphabyte.svg"}
@@ -17,7 +17,7 @@ export default function Categories() {
                   height={60}
                 />
               </div>
-              <div className="w-2/3">{item.name}</div>
+              <div className="w-2/3 uppercase font-bold ">{item.name}</div>
             </div>
           </Link>
         </div>
